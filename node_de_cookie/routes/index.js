fb42885bc21e1a8969e9e0d073fc8c1f60ddf3dc
zurustar <zurustar@users.noreply.github.com>
2021-10-23 22:22:04 +0900

    node.jsでクッキーを使う練習

---
+++ node_de_cookie/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  let username = req.cookies.username;
+  console.log("username = ", username);
+  res.render('index', {username: username});
+
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', {message: ""});
+})
+
+router.post('/login', function(req, res){
+  let username = req.body.username;
+  let password = req.body.password;
+  if((username === 'user01') && (password === 'user01')) {
+    res.cookie('username', username, {
+      maxAge: 30 * 1000,
+      httpOnly: false
+    })
+    res.render('index', {username: username});
+  } else {
+    res.render('login', {message: "なんか間違ってます。"});
+  }
+})
+
+router.get('/logoff', function(req,res) {
+  res.clearCookie("username");
+  res.render('logoff');
+})
+
+module.exports = router;
